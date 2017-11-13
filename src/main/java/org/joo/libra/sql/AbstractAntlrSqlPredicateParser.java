879b4a1a47beb711de62d1b66fd9e01c8a9529b5
griever <griever@PC01NH3V.sg.oracle.com>
2017-11-13 14:49:22 +0800

    refactor

---
+++ src/main/java/org/joo/libra/sql/AbstractAntlrSqlPredicateParser.java
@@ -0,0 +1,34 @@
+package org.joo.libra.sql;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.Parser;
+import org.joo.libra.Predicate;
+
+public abstract class AbstractAntlrSqlPredicateParser<L extends Lexer, P extends Parser> implements SqlPredicateParser {
+
+	@Override
+	public Predicate parse(String predicate) {
+		CharStream stream = CharStreams.fromString(predicate);
+		
+		Lexer lexer = createLexer(stream);
+		
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		
+		P parser = createParser(tokens);
+		
+		ExpressionNode node = doParse(parser);
+		if (node == null) {
+			return null;
+		}
+		return node.buildPredicate();
+	}
+
+	protected abstract L createLexer(CharStream stream);
+
+	protected abstract P createParser(CommonTokenStream tokens);
+	
+	protected abstract ExpressionNode doParse(P parser);
+}
--- src/main/java/org/joo/libra/sql/AntlrSqlPredicateParser.java
+++ src/main/java/org/joo/libra/sql/AntlrSqlPredicateParser.java
@@ -2,38 +2,33 @@ package org.joo.libra.sql;
 
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ConsoleErrorListener;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
-import org.joo.libra.Predicate;
 import org.joo.libra.sql.antlr.SqlLexer;
 import org.joo.libra.sql.antlr.SqlParser;
 import org.joo.libra.support.MalformedSyntaxException;
 
-public class AntlrSqlPredicateParser implements SqlPredicateParser {
+public class AntlrSqlPredicateParser extends AbstractAntlrSqlPredicateParser<SqlLexer, SqlParser> {
 
-	@Override
-	public Predicate parse(String predicate) {
-		SqlErrorListener errorListener = new SqlErrorListener();
-		CharStream stream = CharStreams.fromString(predicate);
+	protected SqlLexer createLexer(CharStream stream) {
 		SqlLexer lexer = new SqlLexer(stream);
 		lexer.removeErrorListener(ConsoleErrorListener.INSTANCE);
-		lexer.addErrorListener(errorListener);
-		
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		
+		lexer.addErrorListener(new SqlErrorListener());
+		return lexer;
+	}
+
+	protected SqlParser createParser(CommonTokenStream tokens) {
 		SqlParser parser = new SqlParser(tokens);
 		parser.removeErrorListener(ConsoleErrorListener.INSTANCE);
-		parser.addErrorListener(errorListener);
-		
+		parser.addErrorListener(new SqlErrorListener());
+		return parser;
+	}
+
+	protected ExpressionNode doParse(SqlParser parser) {
 		SqlVisitor visitor = new SqlVisitor();
-		ExpressionNode node = visitor.visit(parser.predicate());
-		if (node == null) {
-			return null;
-		}
-		return node.buildPredicate();
+		return visitor.visit(parser.predicate());
 	}
 }
 
