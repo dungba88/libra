46902c272957aa07666c873b5e77c9e64b474f4c
griever <griever@PC01NH3V.sg.oracle.com>
2017-11-13 21:14:08 +0800

    refactor

--- src/main/java/org/joo/libra/sql/SqlPredicate.java
+++ src/main/java/org/joo/libra/sql/SqlPredicate.java
@@ -1,13 +1,8 @@
 package org.joo.libra.sql;
 
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.joo.libra.Predicate;
 import org.joo.libra.PredicateContext;
 import org.joo.libra.common.CompositionPredicate;
-import org.joo.libra.sql.antlr.SqlLexer;
-import org.joo.libra.sql.antlr.SqlParser;
 import org.joo.libra.support.MalformedSyntaxException;
 import org.joo.libra.support.PredicateExecutionException;
 
@@ -20,39 +15,25 @@ public class SqlPredicate extends CompositionPredicate {
 	private Predicate predicate;
 
 	public SqlPredicate(String predicate) {
-		this.predicate = parse(predicate);
+		this(predicate, new AntlrSqlPredicateParser());
 	}
-
-	private Predicate parse(String predicate) {
-		try {
-			CharStream stream = CharStreams.fromString(predicate);
-			SqlLexer lexer = new SqlLexer(stream);
-			
-			CommonTokenStream tokens = new CommonTokenStream(lexer);
-			
-			SqlParser parser = new SqlParser(tokens);
-			SqlVisitor visitor = new SqlVisitor();
-			ExpressionNode node = visitor.visit(parser.predicate());
-			if (node == null) {
-				error = true;
-				return null;
-			}
-			return node.buildPredicate();
-		} catch(MalformedSyntaxException ex) {
-			error = true;
-			cause = ex;
-			return null;
-		}
+	
+	public SqlPredicate(String predicate, SqlPredicateParser parser) {
+		this.predicate = parser.parse(predicate);
 	}
 
 	@Override
 	public boolean satisfiedBy(PredicateContext context) throws PredicateExecutionException {
-		if (error) return false;
+		if (error || predicate == null) return false;
 		try {
 			return predicate != null ? predicate.satisfiedBy(context) : false;
 		} catch(Throwable ex) {
 			throw new PredicateExecutionException("Exception while executing SQL predicate", ex);
 		}
+	}
+	
+	public void checkForErrorAndThrow() {
+		if (error) throw cause;
 	}
 	
 	public boolean hasError() {
