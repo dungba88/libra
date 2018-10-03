3062dd17bd03f261841aeb26c7a32345ea446df7
anbui <anbui@ANBUI-IN.mshome.net>
2018-10-03 12:52:38 +0800

    compact AND & OR expression node

--- src/main/java/org/joo/libra/sql/node/AndExpressionNode.java
+++ src/main/java/org/joo/libra/sql/node/AndExpressionNode.java
@@ -1,5 +1,9 @@
 package org.joo.libra.sql.node;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.joo.libra.Predicate;
 import org.joo.libra.logic.AndPredicate;
 
@@ -7,6 +11,24 @@ public class AndExpressionNode extends InfixExpressionNode {
 
     @Override
     public Predicate buildPredicate() {
-        return new AndPredicate(getLeft().buildPredicate(), getRight().buildPredicate());
+    	List<Predicate> predicates = compactExpressionPath();
+        return new AndPredicate(predicates.toArray(new Predicate[0]));
     }
+
+	private List<Predicate> compactExpressionPath() {
+		List<Predicate> predicates = new ArrayList<>();
+    	Predicate leftPredicate = getLeft().buildPredicate();
+    	if (leftPredicate instanceof AndPredicate) {
+    		predicates.addAll(Arrays.asList(((AndPredicate) leftPredicate).getPredicates()));
+    	} else {
+    		predicates.add(leftPredicate);
+    	}
+    	Predicate rightPredicate = getRight().buildPredicate();
+    	if (rightPredicate instanceof AndPredicate) {
+    		predicates.addAll(Arrays.asList(((AndPredicate) rightPredicate).getPredicates()));
+    	} else {
+    		predicates.add(rightPredicate);
+    	}
+		return predicates;
+	}
 }
--- src/main/java/org/joo/libra/sql/node/OrExpressionNode.java
+++ src/main/java/org/joo/libra/sql/node/OrExpressionNode.java
@@ -1,5 +1,9 @@
 package org.joo.libra.sql.node;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.joo.libra.Predicate;
 import org.joo.libra.logic.OrPredicate;
 
@@ -7,6 +11,24 @@ public class OrExpressionNode extends InfixExpressionNode {
 
     @Override
     public Predicate buildPredicate() {
-        return new OrPredicate(getLeft().buildPredicate(), getRight().buildPredicate());
+    	List<Predicate> predicates = compactExpressionPath();
+        return new OrPredicate(predicates.toArray(new Predicate[0]));
     }
+    
+    private List<Predicate> compactExpressionPath() {
+		List<Predicate> predicates = new ArrayList<>();
+    	Predicate leftPredicate = getLeft().buildPredicate();
+    	if (leftPredicate instanceof OrPredicate) {
+    		predicates.addAll(Arrays.asList(((OrPredicate) leftPredicate).getPredicates()));
+    	} else {
+    		predicates.add(leftPredicate);
+    	}
+    	Predicate rightPredicate = getRight().buildPredicate();
+    	if (rightPredicate instanceof OrPredicate) {
+    		predicates.addAll(Arrays.asList(((OrPredicate) rightPredicate).getPredicates()));
+    	} else {
+    		predicates.add(rightPredicate);
+    	}
+		return predicates;
+	}
 }
