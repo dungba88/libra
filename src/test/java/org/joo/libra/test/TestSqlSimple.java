821576a86663a2d920c9d626353b7c32d693d1ee
anbui <anbui@ANBUI-IN.mshome.net>
2018-10-04 16:31:45 +0800

    add test case [ci skip]

--- src/test/java/org/joo/libra/test/TestSqlSimple.java
+++ src/test/java/org/joo/libra/test/TestSqlSimple.java
@@ -104,6 +104,7 @@ public class TestSqlSimple {
 		list.add(new Object[] { "sqrt 4 == 2", true });
 		list.add(new Object[] { "sqrt (2 * 5 + 6) == 4", true });
 		list.add(new Object[] { "sqrt (4) - 1 = 1", true });
+		list.add(new Object[] { "sqrt (4) - 1 * 2 = 0", true });
 
 		return list;
 	}
